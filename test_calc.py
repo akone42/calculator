e6afcbae6feca8604e8aadf5d58cdb6044d9cf92
Alou Kone <konealoubadra@gmail.com>
2025-06-30 17:33:34 +0000

    fixed error

--- test_calc.py
+++ test_calc.py
@@ -15,9 +15,12 @@ class TestCalc(unittest.TestCase):
     def test_mul(self):
         self.assertEqual(self.calc.mul(2, 3), 6)
 
-   def test_div(self):
-        self.assertEqual(self.calc.div(4,2),2)
-   def test_pow(self):
-        self.assertEqual(self.calc.pow(2,3),8)
-   def test_sqrt(self):
-        self.assertEqual(self.calc.sqrt(16),4)
+    def test_div(self):
+        self.assertEqual(self.calc.div(4, 2), 2)
+
+    def test_pow(self):
+        self.assertEqual(self.calc.pow(2, 3), 8)
+
+    def test_sqrt(self):
+        self.assertEqual(self.calc.sqrt(16), 4)
+
